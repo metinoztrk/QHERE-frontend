74b0fde0198c8b9eb778500d36e00395f3454854
metinoztrk <m.ozturk721@gmail.com>
2018-11-25 19:19:27 +0300

    ClassQhere componentine socket eklendi

--- qhere-frontend/src/components/ClassQhere.js
+++ qhere-frontend/src/components/ClassQhere.js
@@ -2,11 +2,17 @@ import React,{Component} from 'react'
 import {connect} from 'react-redux'
 import QRCode from 'react-qr-code';
 import {Redirect} from 'react-router-dom'
+import io from 'socket.io-client';
+import {List, Divider} from 'semantic-ui-react'
+ 
+const socket = io('http://localhost:3001/');
+
 class ClassQhere extends Component{
 
     state={
         classid:"",
-        finished:false
+        finished:false,
+        socketStudents:[],
     }
 
     componentWillMount(){
@@ -28,11 +34,29 @@ class ClassQhere extends Component{
         })
     }
 
+    componentWillUnmount(){
+        socket.emit('deleteClass',{ classId:this.state.classid});
+    }
+
+    shouldComponentUpdate(){
+        socket.on('managerSend', (data) => {
+            if(data!==this.state.socketStudents[this.state.socketStudents.length-1])
+            {
+                this.setState(prevState=>({
+                    socketStudents:[...prevState.socketStudents, data]
+                })) 
+               
+            }
+        });
+        return true
+    }
+
     render() {
-      console.log(this.props)
+      console.log(this.state)
+        socket.emit('createClass',{ classId:this.state.classid});
         
-        const Qr=(
-            <div>
+        const Qr=(       
+                <div >
                     <h1>
                         React QR Codes
                     </h1>
@@ -43,7 +67,27 @@ class ClassQhere extends Component{
                     fgColor='#000'
                     level='H'
                     />
+                <div style={style.div}>
+                <h1>
+                        Yoklamaya Katılan Öğrenciler
+                </h1>
+                {
+                    
+                    this.state.socketStudents.map((student)=>
+                        <List key={student.classId} divided relaxed>
+                        <List.Item>
+                        <List.Content floated='left'>
+                            <List.Header>{student.fullName+" "+student.schoolNumber}</List.Header>
+                        </List.Content>
+                        </List.Item>
+                        <Divider/>
+                        </List> 
+                    )
+
+                }
                 </div>
+                </div>
+                
         )
 
         return (
@@ -60,6 +104,20 @@ const mapStateToProps=(state)=>{
         classes:state.manager.classes,
         lastQrId:state.manager.lastQrId
     }
+}
+
+const style={
+
+    div:{
+        margin:'auto',
+        marginTop:50,
+        marginLeft:300,
+        marginRight:300,
+        borderStyle: 'groove',
+        borderRadius: 25,
+        padding:20
+    }
+    
 }
 
 export default connect(mapStateToProps) (ClassQhere);
